022262730d88e004c5776b2e734451986fceaa18
N.I <iworksWeb@gmail.com>
2025-03-13 02:38:25 +0900

    client use grahQL to client/src/App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,39 @@
 import "./App.css";
+import { gql, useQuery } from "@apollo/client";
+
+const booksQuery = gql`
+  query {
+    test {
+      title
+      author
+    }
+  }
+`;
+
+function Books() {
+  const { loading, error, data } = useQuery(booksQuery);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return (
+    <div>
+      <h2>Books</h2>
+      <ul>
+        {data.test.map((book: any) => (
+          <li key={book.title}>
+            {book.title} by {book.author}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>GraphQL Client</h1>
+      <Books />
     </div>
   );
 }
